778f7f7ca1eed8b005413ecbd4db79a1c5e3246a
Karthik Thejendra <119528503+KarthikT23@users.noreply.github.com>
2024-06-26 19:45:31 +0530

    Add files via upload

---
+++ RGB_sensor_red_detect/RGB_sensor_red_detect.ino
@@ -0,0 +1,64 @@
+#include <Wire.h>
+#include "Adafruit_TCS34725.h"
+
+/* Initialise with default values (int time = 2.4ms, gain = 1x) */
+Adafruit_TCS34725 tcs = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_614MS, TCS34725_GAIN_1X);
+
+// Set the threshold value for the red channel (adjust as needed)
+const uint16_t RED_THRESHOLD = 6;
+const uint16_t GREEN_THRESHOLD = 6;
+const uint16_t BLUE_THRESHOLD = 6;
+
+void setup(void) {
+  Serial.begin(9600);
+
+  if (tcs.begin()) {
+    Serial.println("Found sensor");
+  } else {
+    Serial.println("No TCS34725 found ... check your connections");
+    while (1);
+  }
+
+  // Now we're ready to get readings!
+}
+
+void loop(void) {
+  uint16_t r, g, b, c;
+
+  tcs.getRawData(&r, &g, &b, &c);
+  
+  // Scale down the raw red channel value for better readability
+  uint8_t redValue = map(r, 0, 65535, 0, 255);
+  Serial.print("R: "); Serial.print(r, DEC); Serial.print("\n");
+  Serial.print("R: "); Serial.print(redValue); Serial.print(" ");
+
+  uint8_t greenValue = map(g, 0, 65535, 0, 255);
+  Serial.print("G: "); Serial.print(g, DEC); Serial.print("\n");
+  Serial.print("G: "); Serial.print(greenValue); Serial.print(" ");
+
+  uint8_t blueValue = map(b, 0, 65535, 0, 255);
+  Serial.print("B: "); Serial.print(b, DEC); Serial.print("\n");
+  Serial.print("B: "); Serial.print(blueValue); Serial.print(" ");
+  
+  // Check if the scaled red channel value is above the threshold
+  if (redValue > RED_THRESHOLD) {
+    Serial.println("Red Detected");
+  } else {
+    Serial.println("No Red Found");
+  }
+
+  if (greenValue > GREEN_THRESHOLD) {
+    Serial.println("Green Detected");
+  } else {
+    Serial.println("No Green Found");
+  }
+
+
+  if (blueValue > BLUE_THRESHOLD) {
+    Serial.println("Blue Detected");
+  } else {
+    Serial.println("No Blue Found");
+  }
+
+  delay(2000);
+}
